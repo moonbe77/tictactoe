f76b34b8adce66cb3ea084a447734cc20a10c8b1
Bernardo Munz <moonbe77@gmail.com>
2020-09-20 19:55:49 +1000

    ADDED random move automatic + delay on nexy move

--- index.js
+++ index.js
@@ -1,78 +1,185 @@
-//toggle between players DONE
-//get cell clicked
-//game logic
-
 const winnerNode = document.getElementById('winner');
 const gameStatsNode = document.getElementById('game_stats');
+const movesNode = document.getElementById('moves');
+const turnNode = document.getElementById('turn');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
 
 let playerX = 'X';
 let playerO = 'O';
-
-const startGame = () => {};
-
-let turn = true;
+let player = 'X';
+let turn = true; //true is man
 let winner = false;
+let moves = 0;
 let game = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
 ];
 
-const addToGame = (row, col, player) => {
-  console.log(game);
-  game[row][col] = player;
-  checkWinner(player);
-};
-
-// const changeTurn = () => {
-//   let player;
-//   turn ? (player = 'X') : (player = 'O');
-//   turn = !turn;
-
-//   return player;
-// };
-
-const handleClick = (e) => {
-  let player;
-
-  if (winner) return;
-  const row = e.target.dataset.row;
-  const col = e.target.dataset.col;
-  if (game[row][col] != '') return console.log('can not click on that cell');
-
-  turn ? (player = 'X') : (player = 'O');
-  e.target.innerText = player;
-  addToGame(row, col, player);
-  turn = !turn;
-
-  if (turn) {
-    player1.classList.add('next_turn');
-    player2.classList.remove('next_turn');
-  } else {
-    player2.classList.add('next_turn');
-    player1.classList.remove('next_turn');
-  }
-};
-
-const resetGame = () => {
+const startGame = () => {
+  console.log('starting game');
   const board = document.getElementById('board').children;
   const boardArray = Array.from(board);
   boardArray.forEach((element) => {
     element.innerText = '';
   });
-  console.log('reset game clicked');
+
   game = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
+
   winnerNode.innerText = ``;
   turn = true;
-  turn = true;
   winner = false;
+  moves = 0;
+  player = 'X';
+  updateGameStats();
+  drawBoard(game);
 };
 
-const declareWinner = (player) => {
+const changePlayer = () => {
+  turn = !turn;
+  turn ? (player = 'X') : (player = 'O');
+};
+
+const addToGame = (row, col, player) => {
+  game[row][col] = player;
+
+  countMoves(game);
+  checkWinner(player);
+  changePlayer();
+  updateGameStats();
+  drawBoard(game);
+  nextMove();
+};
+
+const drawBoard = (board) => {
+  board[0].forEach((e, i) => {
+    if (e != '') {
+      switch (i) {
+        case 0:
+          document.getElementById('1').innerText = e;
+          break;
+        case 1:
+          document.getElementById('2').innerText = e;
+          break;
+        case 2:
+          document.getElementById('3').innerText = e;
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+  board[1].forEach((e, i) => {
+    if (e != '') {
+      switch (i) {
+        case 0:
+          document.getElementById('4').innerText = e;
+          break;
+        case 1:
+          document.getElementById('5').innerText = e;
+          break;
+        case 2:
+          document.getElementById('6').innerText = e;
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+  board[2].forEach((e, i) => {
+    if (e != '') {
+      switch (i) {
+        case 0:
+          document.getElementById('7').innerText = e;
+          break;
+        case 1:
+          document.getElementById('8').innerText = e;
+          break;
+        case 2:
+          document.getElementById('9').innerText = e;
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+};
+
+const aiMove = () => {
+  let emptyCells = [];
+
+  game.forEach((row, index) => {
+    //rows
+    row.forEach((e, i) => {
+      // cols
+      e == '' && emptyCells.push([index, i]);
+    });
+  });
+
+  const pickRandom = Math.floor(Math.random() * Math.floor(emptyCells.length));
+
+  return [emptyCells[pickRandom][0], emptyCells[pickRandom][1]];
+};
+
+const nextMove = () => {
+  console.log('next move');
+  if (turn) return;
+  if (winner) return;
+  const move = aiMove();
+  console.log(move);
+
+  const timer = setTimeout(() => {
+    console.log('random move called');
+    addToGame(move[0], move[1], player);
+  }, 1000);
+
+  // clearTimeout(timer);
+};
+
+const updateGameStats = () => {
+  movesNode.innerText = 9 - moves;
+
+  if (turn) {
+    turnNode.innerText = `${playerX} is playing`;
+    player1.classList.add('next_turn');
+    player2.classList.remove('next_turn');
+  } else {
+    turnNode.innerText = `${playerO} is playing`;
+    player2.classList.add('next_turn');
+    player1.classList.remove('next_turn');
+  }
+  if (winner) {
+    turnNode.innerText = `-`;
+    player2.classList.remove('next_turn');
+    player1.classList.remove('next_turn');
+  }
+};
+
+const handleClick = (e) => {
+  if (winner) return;
+  const row = e.target.dataset.row;
+  const col = e.target.dataset.col;
+  if (game[row][col] != '') return console.log('can not click on that cell');
+
+  addToGame(row, col, player, e);
+};
+
+const declareWinner = (player, draw) => {
+  if (draw) {
+    winnerNode.innerText = `the game is draw`;
+    player2.classList.remove('next_turn');
+    player1.classList.remove('next_turn');
+    return;
+  }
+
   winner = player === 'X' ? playerX : playerO;
   if (winner) {
     winnerNode.innerText = `${winner} is the winner`;
@@ -117,6 +224,14 @@ const checkWinner = (player) => {
   if (r3c1 && r2c2 && r1c3) {
     declareWinner(player);
   }
+
+  if (moves === 9) {
+    declareWinner(null, true);
+  }
+
+  // if (!winner && moves < 9 && turn == false) {
+  //   aiMove();
+  // }
 };
 
 const addNameToPlayer = (e) => {
@@ -133,13 +248,20 @@ const addNameToPlayer = (e) => {
   }
 };
 
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
+const countMoves = (board) => {
+  moves = 0;
+  board.forEach((e, i) => {
+    e.forEach((e) => {
+      e != '' && moves++;
+    });
+  });
+};
+
 player1.addEventListener('click', addNameToPlayer);
 player2.addEventListener('click', addNameToPlayer);
 document
   .getElementById('reset_game_button')
-  .addEventListener('click', resetGame);
+  .addEventListener('click', startGame);
 
 document.getElementById('1').addEventListener('click', handleClick);
 document.getElementById('2').addEventListener('click', handleClick);
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # GAME TICK TAC TOE on vanilla JS
 
 ## Coding on js playing with logical thinking
-
-playable
-** [try it here >>] (https://moonbe77.github.io/tictactoe/)
+#### man vs man is playable, 
+..*  [try it here >>] (https://moonbe77.github.io/tictactoe/)
