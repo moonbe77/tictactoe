6f9643d397f4dbc631652de92e367661e32ccbe9
Bernardo Munz <moonbe77@gmail.com>
2020-09-20 13:53:36 +1000

    FEATURE: showing player next turn

--- index.js
+++ index.js
@@ -4,47 +4,54 @@
 
 const winnerNode = document.getElementById('winner');
 const gameStatsNode = document.getElementById('game_stats');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-let playerX, playerY;
 
-const startGame = () => {
-  playerX = prompt('Enter player X name') || 'X';
-  playerO = prompt('Enter player O name') || 'O';
+let playerX = 'X';
+let playerO = 'O';
 
-  player1.innerHTML = `P1: ${playerX}`;
-  player2.innerHTML = `P2: ${playerO}`;
-};
+const startGame = () => {};
 
+let turn = true;
+let winner = false;
 let game = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
 ];
 
-let turn = true;
-let winner = false;
-
 const addToGame = (row, col, player) => {
+  console.log(game);
   game[row][col] = player;
   checkWinner(player);
 };
 
-// drawGame();
+// const changeTurn = () => {
+//   let player;
+//   turn ? (player = 'X') : (player = 'O');
+//   turn = !turn;
+
+//   return player;
+// };
 
 const handleClick = (e) => {
-  if (winner) return;
+  let player;
 
+  if (winner) return;
   const row = e.target.dataset.row;
   const col = e.target.dataset.col;
-  if (game[row][col] != '') return console.log('cell already with value');
+  if (game[row][col] != '') return console.log('can not click on that cell');
 
-  let player;
   turn ? (player = 'X') : (player = 'O');
   e.target.innerText = player;
+  addToGame(row, col, player);
   turn = !turn;
 
-  addToGame(row, col, player);
+  if (turn) {
+    player1.classList.add('next_turn');
+    player2.classList.remove('next_turn');
+  } else {
+    player2.classList.add('next_turn');
+    player1.classList.remove('next_turn');
+  }
 };
 
 const resetGame = () => {
@@ -69,22 +76,23 @@ const declareWinner = (player) => {
   winner = player === 'X' ? playerX : playerO;
   if (winner) {
     winnerNode.innerText = `${winner} is the winner`;
-  } 
+    player2.classList.remove('next_turn');
+    player1.classList.remove('next_turn');
+  }
 };
+
 const checkWinner = (player) => {
-  let r1c1, r1c2, r1c3, r2c1, r2c2, r2c3, r3c1, r3c2, r3c3;
+  let r1c1 = game[0][0] === player;
+  let r1c2 = game[0][1] === player;
+  let r1c3 = game[0][2] === player;
 
-  r1c1 = game[0][0] === player;
-  r1c2 = game[0][1] === player;
-  r1c3 = game[0][2] === player;
+  let r2c1 = game[1][0] === player;
+  let r2c2 = game[1][1] === player;
+  let r2c3 = game[1][2] === player;
 
-  r2c1 = game[1][0] === player;
-  r2c2 = game[1][1] === player;
-  r2c3 = game[1][2] === player;
-
-  r3c1 = game[2][0] === player;
-  r3c2 = game[2][1] === player;
-  r3c3 = game[2][2] === player;
+  let r3c1 = game[2][0] === player;
+  let r3c2 = game[2][1] === player;
+  let r3c3 = game[2][2] === player;
 
   if (
     (r1c1 && r1c2 && r1c3) ||
@@ -111,6 +119,24 @@ const checkWinner = (player) => {
   }
 };
 
+const addNameToPlayer = (e) => {
+  const player = e.target.id;
+  console.log(player);
+
+  if (player === 'player1') {
+    playerX = prompt('Enter player X name') || 'X';
+    player1.innerHTML = `P1: ${playerX}`;
+  }
+  if (player === 'player2') {
+    playerO = prompt('Enter player X name') || 'X';
+    player2.innerHTML = `P2: ${playerO}`;
+  }
+};
+
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+player1.addEventListener('click', addNameToPlayer);
+player2.addEventListener('click', addNameToPlayer);
 document
   .getElementById('reset_game_button')
   .addEventListener('click', resetGame);
