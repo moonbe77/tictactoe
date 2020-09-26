a00fcd46b1136d2b75fbddb873cab29baa5e5d30
Bernardo Munz <moonbe77@gmail.com>
2020-09-26 18:54:19 +1000

    adding minimax

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const winnerNode = document.getElementById('winner');
+const modalNode = document.querySelector('.modal');
 const gameStatsNode = document.getElementById('game_stats');
 const movesNode = document.getElementById('moves');
 const turnNode = document.getElementById('turn');
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
+const cellElements = document.querySelectorAll('.cell');
 
 let playerX = 'X';
 let playerO = 'O';
-let player = 'X';
+let player = playerX;
 let turn = true; //true is man
 let isWinner = false;
 let moves = 0;
@@ -16,9 +18,20 @@ let game = [
   ['', '', ''],
   ['', '', ''],
 ];
+cellElements.forEach((e) => {
+  e.addEventListener('click', handleClick, { once: true });
+});
+
+function handleClick(e) {
+  if (isWinner) return;
+  const row = e.target.dataset.row;
+  const col = e.target.dataset.col;
+
+  addToGame(row, col, player);
+}
 
 const startGame = () => {
-  console.log('starting game');
+  hideModal();
   const board = document.getElementById('board').children;
   const boardArray = Array.from(board);
   boardArray.forEach((element) => {
@@ -35,21 +48,22 @@ const startGame = () => {
   turn = true;
   isWinner = false;
   moves = 0;
-  player = 'X';
+  player = playerX;
   updateGameStats();
   drawBoard(game);
 };
 
 const changePlayer = () => {
   turn = !turn;
-  turn ? (player = 'X') : (player = 'O');
+  turn ? (player = playerX) : (player = playerO);
+  // console.log(player);
 };
 
 const addToGame = (row, col, player) => {
   game[row][col] = player;
 
   countMoves(game);
-  checkWinner(game);
+  checkWinner(game, player);
   changePlayer();
   updateGameStats();
   drawBoard(game);
@@ -113,10 +127,10 @@ const drawBoard = (board) => {
   });
 };
 
-const aiMove = () => {
+const availableMoves = (board) => {
   let emptyCells = [];
 
-  game.forEach((row, index) => {
+  board.forEach((row, index) => {
     //rows
     row.forEach((e, i) => {
       // cols
@@ -124,20 +138,81 @@ const aiMove = () => {
     });
   });
 
-  const pickRandom = Math.floor(Math.random() * Math.floor(emptyCells.length));
+  return emptyCells;
+};
 
-  return [emptyCells[pickRandom][0], emptyCells[pickRandom][1]];
+const aiMove = () => {
+  const posibleMoves = availableMoves();
+  const pickRandom = Math.floor(
+    Math.random() * Math.floor(posibleMoves.length)
+  );
+
+  return [posibleMoves[pickRandom][0], posibleMoves[pickRandom][1]];
+};
+
+const minimax = (game, depth, player) => {
+  const posibleMoves = availableMoves();
+  if (checkWinner(game, player)) {
+    return move;
+  }
+
+  if (player === playerO) {
+    let bestScore = Infinity;
+
+    posibleMoves.forEach((element) => {
+      if (checkWinner(game, player)) {
+        return {
+          score: -10,
+        };
+      } else if (checkWinner(game, player)) {
+        return {
+          score: -10,
+        };
+      } else if (posibleMoves.length === 0) {
+        return {
+          score: 0,
+        };
+      }
+    });
+  } else {
+    if (checkWinner(game, player)) {
+      return {
+        score: -10,
+      };
+    } else if (checkWinner(game, player)) {
+      return {
+        score: -10,
+      };
+    } else if (posibleMoves.length === 0) {
+      return {
+        score: 0,
+      };
+    }
+  }
 };
 
 const nextMove = () => {
-  console.log('next move');
+  console.log(game);
   if (turn) return;
   if (isWinner) return;
+  const copyGame = [...game];
+  const posibleMoves = availableMoves(copyGame);
+
+  posibleMoves.forEach((element) => {
+    console.log('element', element);
+
+    copyGame[element[0]][element[1]] = playerO;
+    console.log('copyGame>', copyGame);
+
+    const bestMove = minimax(copyGame, playerO);
+    console.log('bestMove', bestMove);
+
+    copyGame[element[0]][element[1]] = '';
+  });
+
   const move = aiMove();
-  console.log(move);
 
   setTimeout(() => {
-    console.log('random move called');
     addToGame(move[0], move[1], player);
   }, 600);
 };
@@ -154,20 +229,11 @@ const updateGameStats = () => {
     player2.classList.add('next_turn');
     player1.classList.remove('next_turn');
   }
-  if (winner) {
+  if (isWinner) {
     turnNode.innerText = `-`;
     player2.classList.remove('next_turn');
     player1.classList.remove('next_turn');
   }
-};
-
-const handleClick = (e) => {
-  if (isWinner) return;
-  const row = e.target.dataset.row;
-  const col = e.target.dataset.col;
-  if (game[row][col] != '') return console.log('can not click on that cell');
-
-  addToGame(row, col, player);
 };
 
 const declareWinner = (player, draw) => {
@@ -178,34 +244,35 @@ const declareWinner = (player, draw) => {
     return;
   }
 
-  let winner = player === 'X' ? playerX : playerO;
+  let winner = player === playerX ? playerX : playerO;
   if (winner) {
     winnerNode.innerText = `${winner} is the winner`;
     player2.classList.remove('next_turn');
     player1.classList.remove('next_turn');
+    showModal();
   }
 };
 
-const checkWinner = (board) => {
-  let r1c1 = board[0][0] === player;
-  let r1c2 = board[0][1] === player;
-  let r1c3 = board[0][2] === player;
+const checkWinner = (board, ply) => {
+  let r1c1 = board[0][0] === ply;
+  let r1c2 = board[0][1] === ply;
+  let r1c3 = board[0][2] === ply;
 
-  let r2c1 = board[1][0] === player;
-  let r2c2 = board[1][1] === player;
-  let r2c3 = board[1][2] === player;
+  let r2c1 = board[1][0] === ply;
+  let r2c2 = board[1][1] === ply;
+  let r2c3 = board[1][2] === ply;
 
-  let r3c1 = board[2][0] === player;
-  let r3c2 = board[2][1] === player;
-  let r3c3 = board[2][2] === player;
+  let r3c1 = board[2][0] === ply;
+  let r3c2 = board[2][1] === ply;
+  let r3c3 = board[2][2] === ply;
 
   if (
     (r1c1 && r1c2 && r1c3) ||
     (r2c1 && r2c2 && r2c3) ||
     (r3c1 && r3c2 && r3c3)
   ) {
-    declareWinner(player);
     isWinner = true;
+    declareWinner(ply);
     return true;
   }
 
@@ -215,17 +282,20 @@ const checkWinner = (board) => {
     (r1c3 && r2c3 && r3c3)
   ) {
     isWinner = true;
-    declareWinner(player);
+    declareWinner(ply);
+    return true;
   }
 
   if (r1c1 && r2c2 && r3c3) {
     isWinner = true;
-    declareWinner(player);
+    declareWinner(ply);
+    return true;
   }
 
   if (r3c1 && r2c2 && r1c3) {
     isWinner = true;
-    declareWinner(player);
+    declareWinner(ply);
+    return true;
   }
 
   if (moves === 9) {
@@ -235,17 +305,18 @@ const checkWinner = (board) => {
 };
 
 const addNameToPlayer = (e) => {
-  const player = e.target.id;
-  console.log(player);
+  const target = e.target.id;
+  // console.log(target);
 
-  if (player === 'player1') {
+  if (target === 'player1') {
     playerX = prompt('Enter player X name') || 'X';
     player1.innerHTML = `P1: ${playerX}`;
   }
-  if (player === 'player2') {
+  if (target === 'player2') {
     playerO = prompt('Enter player X name') || 'X';
     player2.innerHTML = `P2: ${playerO}`;
   }
+  startGame();
 };
 
 const countMoves = (board) => {
@@ -257,20 +328,17 @@ const countMoves = (board) => {
   });
 };
 
+const showModal = () => {
+  modalNode.classList.add('show');
+};
+const hideModal = () => {
+  modalNode.classList.remove('show');
+};
+
 player1.addEventListener('click', addNameToPlayer);
 player2.addEventListener('click', addNameToPlayer);
 document
   .getElementById('reset_game_button')
   .addEventListener('click', startGame);
-
-document.getElementById('1').addEventListener('click', handleClick);
-document.getElementById('2').addEventListener('click', handleClick);
-document.getElementById('3').addEventListener('click', handleClick);
-document.getElementById('4').addEventListener('click', handleClick);
-document.getElementById('5').addEventListener('click', handleClick);
-document.getElementById('6').addEventListener('click', handleClick);
-document.getElementById('7').addEventListener('click', handleClick);
-document.getElementById('8').addEventListener('click', handleClick);
-document.getElementById('9').addEventListener('click', handleClick);
 
 startGame();
